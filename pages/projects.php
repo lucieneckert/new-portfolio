8fdeb3b5f7bab8b89bda27de2bcb5a0d9d3aa580
lucieneckert <lucien.eckert@gmail.com>
2022-08-11 01:15:00 +0000

    Dressed up the juicebox browser version page a bit

--- pages/projects.php
+++ pages/projects.php
@@ -49,7 +49,7 @@ $tools = exec_query($db, $tools_query)->fetchAll();
                             <img src="public/images/project-thumbs/jb-banner.png" alt="A Small Game About Juiceboxes" class="featured-project-img">
                             <p>My first solo game project made using Godot Engine. Developed over three months in quarantine, the focus of this project was to experiment with game "juice" (hence the name) and familiarize myself with the process of developing a game from start to finish.</p>
                             <div>
-                            <a href="https://twitter.com/sprodsoft" class='projects-button' target="_blank">
+                            <a href="/jb-browser-ver" class='projects-button' target="_blank">
                             <span>🎮 Play in-browser</span>
                             </a>
                             <a href="/view?project=jb" class='projects-button'>
